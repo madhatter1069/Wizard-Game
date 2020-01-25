9d8a105ed403058e9531b8e25729a3361b961ad9
Zhehao Xie <zhehaox@uci.edu>
2020-01-25 15:39:46 -0800

    First commit
    Unity Project. Basic Player, Enemy and Bullet classes

---
+++ Assets/Scripts/BaseBullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum spellType { def,ice,fire};
+
+
+
+public class BaseBullet : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector2 moveDirection;
+    public float damage;
+
+    float lifeTime;
+    float beginTime;
+
+    void Start()
+    {
+        beginTime = Time.time;
+        lifeTime = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckLifeTime();
+        
+    }
+
+    private void Move()
+    {
+        transform.transform.Translate(moveDirection*moveSpeed*Time.deltaTime);
+    }
+
+    private void CheckLifeTime()
+    {
+        if (Time.time - beginTime > lifeTime)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BaseEnemy : MonoBehaviour
+{
+    public float enemyId;
+    public float health;
+    public GameObject targetPlayer;
+    public spellType weakSpellType;
+    public spellType resistedSpellType;
+    public spellType ImmuneSpellType;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
