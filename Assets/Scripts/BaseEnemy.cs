60df5ccaff0a345d8421bf39241196ccef3608a2
Zhehao Xie <zhehaox@uci.edu>
2020-02-08 13:44:29 -0800

    add slime attack, animation, fix navigation

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -8,22 +8,76 @@ public class BaseEnemy : MonoBehaviour
 {
     public float enemyId;
     public float health;
-    public GameObject targetPlayer;
+    public float attackDamage;
+ 
     public spellType weakSpellType;
     public spellType resistedSpellType;
-    public spellType ImmuneSpellType;
+    public spellType immuneSpellType;
     public float weaknessDamageFactor;
     public float resistedDamageFactor;
 
+    public float targetingRange;
+    public float loseTargetingRange;
+    public float attackRange;
+    public float attackCD;
+
+    [SerializeField]GameObject targetPlayer;
+    private bool _isMoving = false;
+    private bool _isAttacking = false;
+    private Vector3 _scale;
+
     void Start()
     {
-    
+        _scale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Attack(); //attack() should be called before move()
+        Move();
+        CheckTargetPlayer();
         
+    }
+
+
+    private void Move()
+    {
+        //find player, start moving and animation
+        if(targetPlayer) {
+            if (!_isMoving && !_isAttacking)
+            {
+                _isMoving = true;
+                StartCoroutine(MoveAnimation());
+            }
+
+            if (targetPlayer.transform.position.x > transform.transform.position.x)
+                GetComponent<SpriteRenderer>().flipX = true;
+            else
+                GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        
+    }
+
+    private void Attack()
+    {
+        if (targetPlayer)
+        {
+            if(Vector2.Distance(targetPlayer.transform.position,transform.position) < attackRange)
+            {
+                if (!_isAttacking)
+                {
+                    _isAttacking = true;
+                    _isMoving = false; //stop moving animation
+                    StartCoroutine(AttackAnimation()); //animate before applying damage in case player is destoryed(dead)
+                    if (targetPlayer.GetComponent<Player>().GetSpellDamage(attackDamage))
+                        targetPlayer = null;
+                    
+                }
+            }
+            //_isAttacking is reset when animation ends
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -46,12 +100,83 @@ public class BaseEnemy : MonoBehaviour
         {
             health -= resistedDamageFactor * default_damage;
         }
-        else if(spell != ImmuneSpellType)
+        else if(spell != immuneSpellType)
         {
             health -= default_damage;
         }
 
         if (health <= 0)
+        {
+            Destroy(GetComponent<AgentHelper>().avatar);
             Destroy(gameObject);
+        }
+    }
+
+    
+
+    private void CheckTargetPlayer()
+    {
+        if (!targetPlayer)
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            for (int i=0;i<players.GetLength(0);++i)
+            {
+                if (Vector2.Distance(players[i].transform.position, transform.position) <= targetingRange) {
+                    targetPlayer = players[i];
+                    return;
+                }
+            }
+        }
+        else
+        {
+            if (Vector2.Distance(targetPlayer.transform.position, transform.position) >= loseTargetingRange)
+            {
+                targetPlayer = null;
+                _isMoving = false;
+                return;
+            }
+        }
+    }
+
+
+    private IEnumerator MoveAnimation()
+    {
+        while (_isMoving)
+        {
+            transform.localScale = new Vector3(_scale.x, 0.9f * _scale.y, _scale.z);
+            yield return new WaitForSeconds(0.15f);
+            transform.localScale = _scale;
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+
+    private IEnumerator AttackAnimation()
+    {
+        Vector3 currentPos = transform.position;
+        Vector3 targetPos = targetPlayer.transform.position;
+        transform.Translate((targetPos - transform.position).normalized * -3f * Time.deltaTime);
+        yield return new WaitForSeconds(0.2f);
+        transform.Translate((targetPos - transform.position).normalized * 3f * Time.deltaTime);
+        yield return new WaitForSeconds(0.05f);
+
+        transform.position = currentPos;
+  
+        yield return new WaitForSeconds(attackCD);
+        _isAttacking = false;
+
+    }
+
+    //accessors
+    public GameObject getTargetPlayer()
+    {
+        return targetPlayer;
+    }
+    public bool isAttacking()
+    {
+        return _isAttacking;
+    }
+    public bool isMoving()
+    {
+        return _isMoving;
     }
 }
