fd49bb753f029977b5e531b2326e536e3ab8270a
Jared <jaredclark11@gmail.com>
2020-02-11 18:45:27 -0800

    fix

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -6,37 +6,22 @@ using UnityEngine;
 public class Spell : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] private int ID = 1;
     [SerializeField] private GameObject spell;
     
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GetPlayer());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        StartCoroutine(wait());
         spell = player.GetComponent<Player>().GetSpell();
         gameObject.GetComponent<Image>().sprite = spell.GetComponent<SpriteRenderer> ().sprite;
     }
 
-    IEnumerator GetPlayer(){
-        yield return new WaitForSeconds(.1f);
-        object[] obj = GameObject.FindObjectsOfType<GameObject>();
-        foreach (object o in obj)
-        {
-            GameObject g = (GameObject) o;
-            if (g.name == "player" + ID.ToString()){
-                player = g;
-            }
-        }
-    }
-
-    IEnumerator wait(){
-        yield return new WaitForSeconds(.2f);
+    public void SetPlay(GameObject playr){
+        player = playr;
     }
 
 }
